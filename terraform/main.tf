f6ac691b6d2009748cd060fd208222bdf7346f48
Ani Emmanuel C. <82587607+Unique8597@users.noreply.github.com>
2026-02-01 21:00:24 +0000

    add a descriptive commit message above.

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,7 +38,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_eventhub_namespace" "eh_namespace" {
-  name                = variable "eventhub_namespace_name", {}
+  name                = var.eventhub_namespace_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "Standard"
