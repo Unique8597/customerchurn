39c47268430606b04d1065313616707f915b9a6c
Ani Emmanuel C. <82587607+Unique8597@users.noreply.github.com>
2026-02-04 12:26:00 +0000

    remove data.storage_utils dependency

--- src/model/utils.py
+++ src/model/utils.py
@@ -2,9 +2,43 @@ from io import BytesIO
 import numpy as np
 import joblib
 import os
-from data.storage_utils import get_blob_service_client
+from azure.identity import ClientSecretCredential
+from azure.storage.blob import BlobServiceClient
+
 
 container_name = os.getenv("AZURE_STORAGE_CONTAINER_NAME")
+
+def get_blob_service_client():
+    """
+    Creates BlobServiceClient using a Service Principal.
+    Requires the following environment variables:
+        AZURE_TENANT_ID
+        AZURE_CLIENT_ID
+        AZURE_CLIENT_SECRET
+        AZURE_STORAGE_ACCOUNT_NAME
+    """
+    tenant_id = os.getenv("TENANT_ID")
+    client_id = os.getenv("CLIENT_ID")
+    client_secret = os.getenv("CLIENT_SECRET")
+    account_name = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
+
+    if not all([tenant_id, client_id, client_secret, account_name]):
+        raise ValueError(
+            "TENANT_ID, CLIENT_ID, CLIENT_SECRET, AZURE_STORAGE_ACCOUNT_NAME must be set"
+        )
+    credential = ClientSecretCredential(
+        tenant_id=tenant_id,
+        client_id=client_id,
+        client_secret=client_secret
+    )
+
+    blob_service_client = BlobServiceClient(
+        account_url=f"https://{account_name}.blob.core.windows.net",
+        credential=credential
+    )
+
+    return blob_service_client
+
 
 blob_service_client = get_blob_service_client()
 
